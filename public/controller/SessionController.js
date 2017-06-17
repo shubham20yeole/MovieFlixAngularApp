d597f7d8f9a7fbd614e8d87579a3c09b809cde95
Shubham yeole <shubham20.yeole@gmail.com>
2017-06-17 06:20:21 -0700

    feat(module-client): Project Init

--- public/controller/SessionController.js
+++ public/controller/SessionController.js
@@ -5,8 +5,9 @@
 	.module('mainApp')
 	.controller('SessionController', SessionController);
 
+	SessionController.$inject = ['LoginService', '$location'];
 
-	function SessionController(){
+	function SessionController(LoginService, $location){
 		console.log('Inside SessionController');
 		var sessionVm = this;
 		sessionVm.currentUser = null;
@@ -15,7 +16,21 @@
 
 		init();
 		function init(){
-			
+			LoginService.checkLogin()
+			.then(function(status){
+				var st = JSON.stringify(status);
+				console.log("Check Login: "+status.data);
+				var type = typeof status.data;
+				if(type==="string"){
+					sessionVm.currentUser = null;
+					sessionVm.message = "No login found";
+					sessionVm.status = false;
+				}else{
+					sessionVm.currentUser = status.data;
+					sessionVm.message = "Welcome "+status.data.fullName;
+					sessionVm.status = true;
+				}
+			})
 		}
 
 		
